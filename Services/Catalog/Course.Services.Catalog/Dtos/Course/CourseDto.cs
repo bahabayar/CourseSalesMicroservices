3fa9597bdc94bf4ddb781e9b810dc1aad57e76ff
Baha <bahabayar@hotmail.com>
2022-09-13 23:34:59 +0300

    Dtos added for CatalogAPI

---
+++ Services/Catalog/Course.Services.Catalog/Dtos/Course/CourseDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Dtos
+{
+    public class CourseDto
+    {
+        public string Id { get; set; }
+        public string CategoryId { get; set; }
+        public CategoryDto Category { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public FeatureDto Feature { get; set; }
+    }
+}
---
+++ Services/Catalog/Course.Services.Catalog/Dtos/Course/CourseUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Dtos.Course
+{
+    public class CourseUpdateDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+        public FeatureDto Feature { get; set; }
+    }
+}
