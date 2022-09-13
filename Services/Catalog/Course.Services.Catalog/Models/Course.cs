fd20f3133a943142f57b97c7c0c65bbcdef33814
Baha <bahabayar@hotmail.com>
2022-09-13 21:37:51 +0300

    Creating Models For CatalogAPI

---
+++ Services/Catalog/Course.Services.Catalog/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        [BsonIgnore]
+        public Category Category { get; set; }
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Feature Feature { get; set; }
+
+
+    }
+}
