d3fe825a67f20f2a25356db2967723a3894e6ecd
bahabayar <bahabayar@hotmail.com>
2023-01-08 18:18:05 +0300

    IdentityResourceOwnerPasswordValidation Added

--- IdentityServer/CoırseSales.IdentityServer/Startup.cs
+++ IdentityServer/CoırseSales.IdentityServer/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CourseSales.IdentityServer.Services;
 
 namespace CourseSales.IdentityServer
 {
@@ -53,6 +54,9 @@ namespace CourseSales.IdentityServer
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
+
+
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidatior>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
